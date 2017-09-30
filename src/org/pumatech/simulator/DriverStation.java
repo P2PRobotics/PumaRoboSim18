0d470b5f47553f7344a27805394146558bba4b9e
VVestin <weston.metzler@gmail.com>
2017-09-30 08:38:28 -0600

    Update DriverStation GUI

--- src/org/pumatech/simulator/DriverStation.java
+++ src/org/pumatech/simulator/DriverStation.java
@@ -3,10 +3,12 @@ package org.pumatech.simulator;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class DriverStation {
 	@SuppressWarnings("unused")
-	private static final int WIDTH = 350, HEIGHT = WIDTH * 690 / 505;
+	private static final int WIDTH = 505, HEIGHT = WIDTH * 690 / 505;
 	private static final double PIXEL = WIDTH / 505d;
 	private static final Color[] RED = {
 			new Color(16,  1, 2),   new Color(45, 1, 4), 
@@ -28,11 +30,10 @@ public class DriverStation {
 		// (clips define the region of the screen which can be drawn on)
 		Rectangle oldClip = g.getClipBounds();
 		
-		// Ugly code to draw the DS GUI. Good Luck debuggin this.
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, (int) (505 * PIXEL), 2000);
-		g.setColor(RED[6]);
-		g.fillRect(0, (int) (112 * PIXEL), (int) (505 * PIXEL), (int) (223 * PIXEL));
+		// Draws the DS GUI
+		rect(g, 0, 0, 505, 2000, Color.BLACK);
+		rect(g, 0, 112, 505, 223, RED[6]);
+		// TODO convert the rest of the drawing to use rect and circle
 		g.setColor(RED[7]);
 		g.fillRect((int) (3 * PIXEL), (int) (339 * PIXEL), (int) (500 * PIXEL), (int) (67 * PIXEL));
 		g.setColor(RED[4]);
@@ -47,6 +48,14 @@ public class DriverStation {
 		g.setClip(oldClip);
 		g.setColor(RED[2]);
 		g.fillRect((int) (3 * PIXEL), (int) (115 * PIXEL), (int) (499 * PIXEL), (int) (141 * PIXEL));
+		rect(g, 3, 262, 247, 71, RED[2]);
+		rect(g, 256, 262, 247, 71, new Color(89, 127, 0));
+		ellipse(g, 189, 123, 127, 126, RED[5]);
+		ellipse(g, 356, 123, 127, 126, RED[5]);
+		ellipse(g, 347, 480, 92, 92, RED[1]);
+		ellipse(g, 392, 591, 29, 29, RED[0]);
+		ellipse(g, 365, 591, 29, 29, RED[0]);
+		rect(g, 376, 592, 30, 29, RED[0]);
 		
 		// Draw line dividing DS from rest of simulation
 		g.setColor(Color.WHITE);
@@ -57,6 +66,21 @@ public class DriverStation {
 		g.setClip((int) (252.5 * PIXEL + 1), 0, 10000, 2000); // Should be big rectangle size
 	}
 
+	private static void rect(Graphics2D g, double x, double y, double width, double height, Color c) {
+		g.setColor(c);
+		g.fill(new Rectangle2D.Double(x * PIXEL, y * PIXEL, width * PIXEL, height * PIXEL));
+	}
+	
+	private static void ellipse(Graphics2D g, double x, double y, double width, double height, Color c) {
+		g.setColor(c);
+		g.fill(new Ellipse2D.Double(x * PIXEL, y * PIXEL, width * PIXEL, height * PIXEL));
+	}
+	
+	private static void circle(Graphics2D g, double x, double y, double radius, Color c) {
+		g.setColor(c);
+		g.fill(new Ellipse2D.Double((x - radius) * PIXEL, (y - radius) * PIXEL, 2 * radius * PIXEL, 2 * radius * PIXEL));
+	}
+	
 	public void toggle() {
 		show = !show;
 	}
